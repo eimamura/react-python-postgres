ff28a28d949fa71a8cecc048f2b972d31c956730
Erielcio Imamura <erielcio00@gmail.com>
2024-06-01 08:36:36 -0300

    fix: cors issue

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://api:8000/:path*'
+            }
+        ];
+    }
+};
 
 export default nextConfig;
